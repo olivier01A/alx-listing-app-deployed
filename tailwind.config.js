49fadcc6af4b7b09be6c16251acf3f1454ebece2
olivier01A <oliviermasengesho590@gmail.com>
2025-11-08 13:29:37 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// module.exports = {
+//   content: [
+//     './pages/**/*.{ts,tsx}',
+//     './components/**/*.{js,ts,jsx,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
